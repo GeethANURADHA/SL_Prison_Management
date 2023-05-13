3d681015f7cb2090a4f50b09fe1013a1ef37aba2
IT20102050 <it20102050@my.sliit.lk>
2023-05-13 09:31:06 +0530

    add controllers

---
+++ server/controllers/general.js
@@ -0,0 +1,30 @@
+import User from "../models/user.js";
+import Status from "../models/Status.js";
+
+
+export const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getDashboardStats = async (req, res) => {
+  try {
+   
+    /* Recent Transactions */
+    const status = await Status.find()
+      .limit(50)
+      .sort({ createdOn: -1 });
+
+    
+    res.status(200).json({
+      status
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
