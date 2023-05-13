e6f7569f887aa7bf2efbdef66adedc35881d06f2
GeethANURADHA <96642560+GeethANURADHA@users.noreply.github.com>
2023-05-13 16:48:17 +0530

    new add for app js

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,18 @@ import React, { useMemo } from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useSelector } from "react-redux";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
+import LogIn from "./login/staff/index.jsx";
 import Layout from "scenes/staffscences/layout";
-import Dashboard from "scenes/staffscences/dashboard";
-import  Staffs from "scenes/staffscences/staffs/index.jsx";
-import  AddStaffs from "scenes/staffscences/staffs/AddStaff.jsx";
+import DashboardStaff from "scenes/staffscences/dashboard";
+import Staffs from "scenes/staffscences/staffs/index.jsx";
+import AddStaffs from "scenes/staffscences/staffs/AddStaff.jsx";
 import EditStaff from "scenes/staffscences/staffs/EditStaff";
 import ResignationStaff from "scenes/staffscences/staffs/ResignationStaff";
 import AddAttendance from "scenes/staffscences/staffs/AddAttendance";
-
-
-
+import RequireAuth from "middleware/staff/RequireAuth.js";
+import RedirectIfAuthenticated from "middleware/staff/RedirectedIfAuthenticated.js";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -24,15 +24,21 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-           <Route element={<Layout />}>
-              <Route path="/" element={<Navigate to="/dashboard" replace />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/Display  Staff Details" element={<Staffs/>}/>
-              <Route path="/Add Staff Member" element={<AddStaffs/>}/>
-              <Route path="/Edit/:_id" element={<EditStaff/>}/>
-              <Route path="/Resignation/:_id" element={<ResignationStaff/>}/>
-              <Route path="/Discard" element={<Staffs/>}/>
-              <Route path="/Staff Attendance" element={<AddAttendance/>}/>
+            <Route
+              path="/"
+              element={<RedirectIfAuthenticated Component={LogIn} />}
+            />
+            <Route element={<RequireAuth Component={Layout} />}>
+              <Route path="/staff dashboard" element={<DashboardStaff />} />
+              <Route path="/Display  Staff Details" element={<Staffs />} />
+              <Route path="/Add Staff Member" element={<AddStaffs />} />
+              <Route path="/Edit/:_id" element={<EditStaff />} />
+              <Route path="/Resignation/:_id" element={<ResignationStaff />} />
+              <Route path="/Discard" element={<Staffs />} />
+              <Route
+                path="/staff attendance/:_id"
+                element={<AddAttendance />}
+              />
             </Route>
           </Routes>
         </ThemeProvider>
