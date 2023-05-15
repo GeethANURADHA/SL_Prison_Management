0e5893e2099e0263cafe7b2f9c2549c5adcd9d20
GeethANURADHA <96642560+GeethANURADHA@users.noreply.github.com>
2023-05-15 16:50:34 +0530

    attendance part update

--- client/src/scenes/staffscences/staffs/AddAttendance.jsx
+++ client/src/scenes/staffscences/staffs/AddAttendance.jsx
@@ -1,50 +1,79 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { TextField, Button, useTheme, Box } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  TextField,
+  Button,
+  useTheme,
+  Box,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import { useNavigate, useParams } from "react-router-dom";
-import { useGetStaffsQuery } from "state/api";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-const AttendanceForm = () => {
-  const [id, setId] = useState("");
-  const [name, setName] = useState("");
-  const [status, setStatus] = useState('present');
-  const [error, setError] = useState(null);
-  const theme = useTheme();
-  const navigate = useNavigate();
-  const { refetch } = useGetStaffsQuery();
+const AddAttendance = () => {
+  const [sid, setId] = useState("");
+  const [sname, setName] = useState("");
+  const [StaffName, setStaffName] = useState("");
+  const [status, setStatus] = useState("Present");
+  const [sdate, setDate] = useState(new Date());
   const { _id } = useParams();
+  console.log("id :", _id);
+  const navigate = useNavigate();
+  const theme = useTheme();
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const getStaffById = async () => {
     try {
-      const response = await axios.post('/api/attendance', {
-        member_id: _id,
-        name,
-        status,
-        date: new Date(),
-      });
+      const response = await axios.get(
+        `http://localhost:5001/client/staffs/${_id}`
+      );
       console.log(response.data);
-      // show success message to user
-      navigate("/");
+      setId(response.data._id);
+      setName(response.data.sname);
+      setStaffName(response.data.StaffName);
     } catch (error) {
-      setError(error.message);
+      console.log(error);
     }
   };
 
+  //attendance new
+  
+  const AttendanceSubmit = async (e) => {
+    e.preventDefault();
+    const body = {
+      sid,
+      sname,
+      StaffName,
+      status,
+      sdate,
+    };
+    try {
+      const response = await axios.post(
+        "http://localhost:5001/client/attendances",
+        body
+      );
+      console.log("add=", response.data);
 
- 
+      navigate("/dashboard");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  useEffect(() => {
+    getStaffById();
+  }, [_id]);
 
   return (
     <div className="columns">
       <div className="column is-half">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={AttendanceSubmit}>
           <Box m="2.5rem 10.5rem">
             <TextField
               label="Staff ID"
               variant="outlined"
               value={_id}
-              onChange={(e) => setId(e.target.value)}
               placeholder="Staff ID"
               fullWidth
               margin="normal"
@@ -52,27 +81,48 @@ const AttendanceForm = () => {
             <TextField
               label="Name"
               variant="outlined"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
+              value={sname}
               placeholder="Name"
               fullWidth
               margin="normal"
-              required
             />
+
             <TextField
-              label="Status"
+              label="Staff Name"
               variant="outlined"
-              value={status}
-              onChange={(e) => setStatus(e.target.value)}
-              placeholder="Status"
+              value={StaffName}
+              placeholder="Name"
               fullWidth
               margin="normal"
-              required
             />
+            {/* <Calendar
+  value={date}
+  onChange={setDate}
+/>*/}
+
+            <DatePicker
+              selected={sdate}
+              dateFormat="yyyy-MM-dd"
+              placeholderText="Date"
+              isClearable
+            />
+
+            <Box sx={{ width: "100%", marginBottom: "1rem" }}>
+              <Select
+                label="Status"
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+                fullWidth
+              >
+                <MenuItem value="Present">Present</MenuItem>
+                <MenuItem value="Absent">Absent</MenuItem>
+                <MenuItem value="Late">Late</MenuItem>
+              </Select>
+            </Box>
             <Button
               sx={{
-                backgroundColor: theme.palette.grey[300],
-                color: theme.palette.background.alt,
+                bgcolor: "grey.300",
+                color: "background.alt",
                 fontSize: "14px",
                 fontWeight: "bold",
                 padding: "10px 20px",
@@ -81,7 +131,6 @@ const AttendanceForm = () => {
               type="submit"
               variant="contained"
               color="primary"
-              href={`/Discard`}
             >
               Add
             </Button>
@@ -92,4 +141,4 @@ const AttendanceForm = () => {
   );
 };
 
-export default AttendanceForm;
+export default AddAttendance;
--- client/src/scenes/staffscences/staffs/AddStaff.jsx
+++ client/src/scenes/staffscences/staffs/AddStaff.jsx
@@ -1,29 +1,34 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { TextField, Button, useTheme, Box ,Select ,MenuItem } from "@mui/material";
-
-
+import {
+  TextField,
+  Button,
+  useTheme,
+  Box,
+  Select,
+  MenuItem,
+} from "@mui/material";
 
 const AddStaff = () => {
-  const [sname,setName]=useState("");
-  const [NIC,setNIC]=useState("");
-  const [Email,setEmail]=useState("");
-  const [designation,setdesignation]=useState("");
-  const [Birthday,setBirthday]=useState("");
-  const [Gender,setGender]=useState("Male");
-  const [Address,setAddress]=useState("");
-  const [BasicSalary,setBasicSalary]=useState("");
-  const [PhoneNumber,setPhoneNumber]=useState("");
-  const [StaffName,setStaffName]=useState("");
-  const navigate=useNavigate();
+  const [sname, setName] = useState("");
+  const [NIC, setNIC] = useState("");
+  const [Email, setEmail] = useState("");
+  const [designation, setdesignation] = useState("");
+  const [Birthday, setBirthday] = useState("");
+  const [Gender, setGender] = useState("Male");
+  const [Address, setAddress] = useState("");
+  const [BasicSalary, setBasicSalary] = useState("");
+  const [PhoneNumber, setPhoneNumber] = useState("");
+  const [StaffName, setStaffName] = useState("");
+  const navigate = useNavigate();
 
   const theme = useTheme();
 
-  const saveStaff = async(e)=>{
+  const saveStaff = async (e) => {
     e.preventDefault();
-    try{
-      await axios.post('http://localhost:5001/client/staffs/', {
+    try {
+      await axios.post("http://localhost:5001/client/staffs/", {
         sname,
         NIC,
         Email,
@@ -34,43 +39,45 @@ const AddStaff = () => {
         BasicSalary,
         PhoneNumber,
         StaffName,
-        
       });
-       navigate("/")
-    }catch(error){
+      navigate("/");
+    } catch (error) {
       console.log(error);
     }
-  }
-  const staffNames = ['John', 'Mary', 'Jane'];
-  
+  };
+  const staffNames = [
+    "Correctional Officers",
+    "Wardens",
+    "Administrative Staff",
+    "Medical Staff",
+    "Psychologists and Counselors",
+    "Support Staff",
+    "Educational Staff",
+  ];
+
   return (
     //react form?
 
-    
     <div className="columns">
       <div className="column is-half">
         <form onSubmit={saveStaff}>
-
-        <Box m="2.5rem 10.5rem">
-
-        
+          <Box m="2.5rem 10.5rem">
             <TextField
               label="Name"
               variant="outlined"
-              value={sname} 
-              onChange={(e)=>setName(e.target.value)}
+              value={sname}
+              onChange={(e) => setName(e.target.value)}
               placeholder="Name"
               fullWidth
               margin="normal"
               required
             />
-          
-          
+
             <TextField
               label="NIC"
               variant="outlined"
-              value={NIC} 
-              onChange={(e)=>setNIC(e.target.value)}
+              value={NIC}
+              onChange={(e) => setNIC(e.target.value)}
               placeholder="NIC"
               fullWidth
               margin="normal"
@@ -79,8 +86,8 @@ const AddStaff = () => {
             <TextField
               label="Email"
               variant="outlined"
-              value={Email} 
-              onChange={(e)=>setEmail(e.target.value)} 
+              value={Email}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="Email"
               fullWidth
               margin="normal"
@@ -89,54 +96,58 @@ const AddStaff = () => {
             <TextField
               label="Designation"
               variant="outlined"
-              value={designation} 
-              onChange={(e)=>setdesignation(e.target.value)} 
+              value={designation}
+              onChange={(e) => setdesignation(e.target.value)}
               placeholder="Designation"
               fullWidth
               margin="normal"
               required
             />
-            
+
             <TextField
               label="Date of Birth"
               variant="outlined"
               type="Date"
-              value={Birthday} 
-              onChange={(e)=>setBirthday(e.target.value)}
+              value={Birthday}
+              onChange={(e) => setBirthday(e.target.value)}
               fullWidth
               margin="normal"
               required
               InputLabelProps={{
-                shrink:true,
+                shrink: true,
               }}
             />
-        <div className="field">
-          <label className="lable">Gender</label>
-          <div className="control">
-            <div className="select  is-fullwidth">
-              <select             value={Gender} 
-            onChange={(e)=>setGender(e.target.value)}>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-              </select>
+            <div className="field">
+              <label className="label">Gender</label>
+              <div className="control">
+                <div className="select is-fullwidth is-rounded is-primary">
+                  <select
+                    value={Gender}
+                    onChange={(e) => setGender(e.target.value)}
+                  >
+                    <option value="">Select Gender</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                  </select>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-             <TextField
+
+            <TextField
               label="Address"
               variant="outlined"
-              value={Address} 
-              onChange={(e)=>setAddress(e.target.value)}
+              value={Address}
+              onChange={(e) => setAddress(e.target.value)}
               placeholder="Address"
               fullWidth
               margin="normal"
               required
-             />
+            />
             <TextField
               label="Basic Salary"
               variant="outlined"
-              value={BasicSalary} 
-              onChange={(e)=>setBasicSalary(e.target.value)}
+              value={BasicSalary}
+              onChange={(e) => setBasicSalary(e.target.value)}
               placeholder="Basic Salary"
               fullWidth
               margin="normal"
@@ -145,61 +156,59 @@ const AddStaff = () => {
             <TextField
               label="Phone Number"
               variant="outlined"
-              value={PhoneNumber} 
-              onChange={(e)=>setPhoneNumber(e.target.value)}
+              value={PhoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
               placeholder="Phone Number"
               fullWidth
               margin="normal"
               required
             />
-      
 
             <Select
-               
-               variant="outlined"
-               id="staff-name-select"
-               value={StaffName}
-               onChange={(e)=>setStaffName(e.target.value)}
-               fullWidth
-              
-               displayEmpty
-               margin="normal"
-               required
-             >
-             <MenuItem value="" disabled>
-              Select Staff Name
-             </MenuItem>
-             {staffNames.map((staffName) => (
-             <MenuItem key={staffName} value={staffName}>
-             {staffName}
-             </MenuItem>
+              variant="outlined"
+              id="staff-name-select"
+              value={StaffName}
+              onChange={(e) => setStaffName(e.target.value)}
+              fullWidth
+              displayEmpty
+              margin="normal"
+              required
+            >
+              <MenuItem value="" disabled>
+                Select Staff Name
+              </MenuItem>
+              {staffNames.map((staffName) => (
+                <MenuItem key={staffName} value={staffName}>
+                  {staffName}
+                </MenuItem>
               ))}
-             </Select>
+            </Select>
 
-        <div className="field">
-          <div className="control">
-            <Button
-            sx={{
-              backgroundColor: theme.palette.secondary.light,
-              color: theme.palette.background.alt,
-              fontSize: "14px",
-              fontWeight: "bold",
-              padding: "10px 20px",
-              float: "right",
-            }}
-            variant="contained"
-            color="primary"
-
-            type="submit" className="button is-success">Save</Button>
-          </div>
-        </div>
-        </Box>
+            <div className="field">
+              <div className="control">
+                <Button
+                  sx={{
+                    backgroundColor: theme.palette.secondary.light,
+                    color: theme.palette.background.alt,
+                    fontSize: "14px",
+                    fontWeight: "bold",
+                    padding: "10px 20px",
+                    float: "right",
+                  }}
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  className="button is-success"
+                >
+                  Save
+                </Button>
+              </div>
+            </div>
+          </Box>
         </form>
-        
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
 export default AddStaff;
