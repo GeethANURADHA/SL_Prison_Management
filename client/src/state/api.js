1595bd976b1870f0a929e9349d47de8248e926b5
GeethANURADHA <96642560+GeethANURADHA@users.noreply.github.com>
2023-05-17 20:54:07 +0530

    api added

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,19 +3,31 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: "adminApi",
-  tagTypes: ["User" ,"Staff"],
-  
+  tagTypes: ["User", "Staff", "Attendance", "Staff Dashboard"],
+
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
       providesTags: ["User"],
     }),
-    getStaffs:  build.query({
+    getStaffs: build.query({
       query: () => "client/staffs/",
-      providesTags: ["Staff"]
-    })
- }),
-
+      providesTags: ["Staff"],
+    }),
+    getAttendance: build.query({
+      query: () => "client/attendances",
+      providesTags: ["Attendance"],
+    }),
+    getDashboardStaffStats: build.query({
+      query: () => "general/dashboard",
+      providesTags: ["Staff Dashboard"],
+    }),
+  }),
 });
 
-export const { useGetUserQuery,useGetStaffsQuery} = api;
+export const {
+  useGetUserQuery,
+  useGetStaffsQuery,
+  useGetAttendanceQuery,
+  useGetDashboardStaffStatsQuery,
+} = api;
