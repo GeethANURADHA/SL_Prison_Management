b35ff0215eb97d53858a6ab0b15c1a8331a7d021
GeethANURADHA <96642560+GeethANURADHA@users.noreply.github.com>
2023-05-19 16:57:40 +0530

    overall stat part add but it is not working

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,5 +1,5 @@
 import  express  from "express";
-import { getStaffs , saveStaff , getStaffById, updateStaff ,deleteStaff } from "../controllers/client.js";
+import { getStaffs , saveStaff , getStaffById, updateStaff ,deleteStaff, saveAttendance,AttendanceSubmit } from "../controllers/client.js";
 
 const router = express.Router();
 
@@ -8,5 +8,11 @@ router.post("/staffs",saveStaff);
 router.get("/staffs/:id",getStaffById)
 router.patch("/staffs/:id",updateStaff)
 router.delete("/staffs/:id", deleteStaff);
+
+
+//attendance form
+router.get("/staffs/:id",getStaffById);
+router.post("/attendances", saveAttendance);
+router.post("/attendances", AttendanceSubmit);
 
 export default router;
